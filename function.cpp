3f3f37b354e80607e4d517d77fe3ab9a96843f1d
Đào Duy Nam <lenkc98@gmail.com>
2017-12-02 01:12:02 +0700

    Update all

--- function.cpp
+++ function.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 //  Check Status
-int fCheckStatus(char position) {
-    if (position == 'T')
-        return 1;
-    else if (position == 'X')
+//  if at treasure: return 2
+//  if on trap: return 1
+int fCheckStatus(int x, int y, int** trapMap) {
+    if ((x == 9) && (y == 9))
         return 2;
+    else if (trapMap[x][y] == 1)
+        return 1;
 }
 
 //  MoveUp
 int moveUp(int& playerPositionX, int& playerPositionY, char** playGround) {
-    // create next position variable
-    char nextPosition;
-
     //  Check if not on the top of screen
     if ( playerPositionX != 0) {
         //  Set back the soil
@@ -20,20 +19,13 @@ int moveUp(int& playerPositionX, int& playerPositionY, char** playGround) {
         //  Change player position
         playerPositionX--;
 
-        //  Check next position
-        nextPosition = playGround[playerPositionX][playerPositionY];
-
         //  Set player position to the map
-        playGround[playerPositionX][playerPositionY] = 'G';
+        playGround[playerPositionX][playerPositionY] = 'H';
     }
-    return fCheckStatus(nextPosition);
 }
 
 //  MoveRight
 int moveRight(int& playerPositionX, int& playerPositionY, char** playGround) {
-    // create next position variable
-    char nextPosition;
-
     //  Check if not on the right of screen
     if ( playerPositionY != 9) {
         //  Set back the soil
@@ -42,20 +34,13 @@ int moveRight(int& playerPositionX, int& playerPositionY, char** playGround) {
         //  Change player position
         playerPositionY++;
 
-        //  Check next position
-        nextPosition = playGround[playerPositionX][playerPositionY];
-
         //  Set player position to the map
-        playGround[playerPositionX][playerPositionY] = 'G';
+        playGround[playerPositionX][playerPositionY] = 'H';
     }
-    return fCheckStatus(nextPosition);
 }
 
 //  MoveDown
 int moveDown(int& playerPositionX, int& playerPositionY, char** playGround) {
-    // create next position variable
-    char nextPosition;
-
     //  Check if not on the bottom of screen
     if ( playerPositionX != 9) {
         //  Set back the soil
@@ -64,20 +49,13 @@ int moveDown(int& playerPositionX, int& playerPositionY, char** playGround) {
         //  Change player position
         playerPositionX++;
 
-        //  Check next position
-        nextPosition = playGround[playerPositionX][playerPositionY];
-
         //  Set player position to the map
-        playGround[playerPositionX][playerPositionY] = 'G';
+        playGround[playerPositionX][playerPositionY] = 'H';
     }
-    return fCheckStatus(nextPosition);
 }
 
 //  MoveLeft
 int moveLeft(int& playerPositionX, int& playerPositionY, char** playGround) {
-    // create next position variable
-    char nextPosition;
-
     //  Check if not on the left of screen
     if ( playerPositionY != 0) {
         //  Set back the soil
@@ -86,28 +64,65 @@ int moveLeft(int& playerPositionX, int& playerPositionY, char** playGround) {
         //  Change player position
         playerPositionY--;
 
-        //  Check next position
-        nextPosition = playGround[playerPositionX][playerPositionY];
-
         //  Set player position to the map
-        playGround[playerPositionX][playerPositionY] = 'G';
+        playGround[playerPositionX][playerPositionY] = 'H';
     }
-    return fCheckStatus(nextPosition);
+}
+
+//  Count traps around you, but not at the corner
+int countTrapAround(int x, int y, int **trapMap) {
+    int trapNumber = 0;
+    if (x != 0)
+        trapNumber += trapMap[x-1][y];
+    if (x != 9)
+        trapNumber += trapMap[x+1][y];
+    if (y != 0)
+        trapNumber += trapMap[x][y-1];
+    if (y != 9)
+        trapNumber += trapMap[x][y+1];
+
+    return trapNumber;
 }
 
 //  Display the playground
 void display(char** playGround) {
     //  INTRO
     std::cout << "-------DUNGEON---CRAWL-------" << "\n\n"
+              << "  Made by Noctis26 \n"
               << "  RULES: \n"
-              << "  1. Use W,A,S,D to move your character (G) \n"
-              << "  2. Avoid Trap (T) \n"
+              << "  1. Use W,A,S,D to move your hero (H) \n"
+              << "  2. Avoid hidden Trap! \n"
               << "  3. Claim the treasure (X) \n"
+              << "      A mystery is waiting for you! \n"
               << "-----------------------------" << '\n';
+
+    // print playground
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             std::cout << ' ' << playGround[i][j];
         }
         std::cout << '\n';
     }
+}
+
+//  FOR DEVELOPER ONLY
+//  DISPLAY TRAPMAP
+void display(int** trapMap) {
+    std::cout << "  TRAP MAP \n";
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            std::cout << ' ' << trapMap[i][j];
+        }
+        std::cout << '\n';
+    }
+}
+
+//  FOR DEVELOPER ONLY
+//  REVEAL TRAP!
+void revealTrap(char **playGround, int **trapMap) {
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++) {
+            if (trapMap[i][j] == 1)
+                playGround[i][j] = 'T';
+        }
 }
--- function.h
+++ function.h
@@ -1,8 +1,12 @@
 //  This file contains prototypes of all
 //  functions in function.cpp file
-int moveUp(int& playerPositionX, int& playerPositionY, char** playGround);
-int moveDown(int& playerPositionX, int& playerPositionY, char** playGround);
-int moveLeft(int& playerPositionX, int& playerPositionY, char** playGround);
+
+int moveUp   (int& playerPositionX, int& playerPositionY, char** playGround);
+int moveDown (int& playerPositionX, int& playerPositionY, char** playGround);
+int moveLeft (int& playerPositionX, int& playerPositionY, char** playGround);
 int moveRight(int& playerPositionX, int& playerPositionY, char** playGround);
 int display(char** playGround);
-int fCheckStatus(char position);
+int fCheckStatus(int x, int y, int** trapMap);
+int countTrapAround(int x, int y, int **trapMap);
+void display(int** trapMap);
+void revealTrap(char **playGround, int **trapMap);
